854b84f10824c8c333d0df24ec9c0c666e8df76b
Billionaire_Developer <lawanijeremiah23@gmail.com>
2024-10-18 17:46:39 +0100

    python

---
+++ proposal.py
@@ -0,0 +1,13 @@
+
+file = open('proposal.txt', 'a')
+file.write("1. PROGRAM TITLE: Personal: Finance Manger\n")
+
+file.write("2. REAL-WORLD PROBLEM: The program will address the problem of managing personal finances effectively, which is a common struggle for many individuals.\n")
+
+file.write("3. WHAT I WILL LEARN: I will learn how to design and implement a comprehensive personal finance management system, including budgeting, investment tracking, retirement planning. I will also gain experience with Python modules such as csv, datetime, and matplotlib.\n")
+
+file.write("4. PYTHON MODULE: csv, datetime, matplotlib, pandas\n")
+
+file.write("5. FUNCTION NAMES: add_transaction, view_transaction, set_budget, check_budget, investment_tracker, retirement_planner, calculate_net_worth, generate_budget_report\n")
+
+file.write("6. TEXT FUNCTION NAME: test_add_transaction, test_view_transaction, test_set_budget, test_check_budget, test_investment_checker, test_retirement_planner, test_calculate_net_worth, test_generate_budget_report")
